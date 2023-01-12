b6afca3cb738a67ca7ee3df8e53e48cf94ec6485
Anakin B <baldock.a@stu.janesville.k12.wi.us>
2023-01-12 11:21:21 -0600

    update

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,8 @@
-function bFunction() {
-    // Get the value of the input field with id="numb"
-    let x = document.getElementById("numb").value;
-    // If x is Not a Number or less than one or greater than 10
-    let text;
-    if (isNaN(x) || x < 1 || x > 10) {
-      text = "Input not valid";
-    } else {
-      text = "Input OK";
-    }
-    document.getElementById("demo").innerHTML = text;
+window.addEventListener('scroll',()=>{
+  let content = document.querySelector('.row');
+  let contentPostion = content.getBoundingClientRect().top;
+  let screenPostion = window.innerHeight;
+  if(contentPostion < screenPostion){
+  content.classList.add('active');
   }
+});
