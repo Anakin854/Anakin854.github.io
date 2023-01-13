56925873306a53b224a2a422edceb71a359e983d
Anakin B <baldock.a@stu.janesville.k12.wi.us>
2023-01-13 11:24:21 -0600

    update

--- JS/script.js
+++ JS/script.js
@@ -6,3 +6,11 @@ window.addEventListener('scroll',()=>{
   content.classList.add('active');
   }
 });
+
+function showDate() {
+  var d = new Date();
+  var curr_date = d.getDate();
+  var curr_month = d.getMonth() + 1; //months are zero based
+  var curr_year = d.getFullYear();
+  document.write(curr_date + "-" + curr_month + "-" + curr_year);
+}
