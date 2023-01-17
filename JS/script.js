a2f7570d4b14a3314f1cc434812e90d9306b619e
Anakin B <baldock.a@stu.janesville.k12.wi.us>
2023-01-17 11:20:57 -0600

    update

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,1 @@
-window.addEventListener('scroll',()=>{
-  let content = document.querySelector('.row');
-  let contentPostion = content.getBoundingClientRect().top;
-  let screenPostion = window.innerHeight;
-  if(contentPostion < screenPostion){
-  content.classList.add('active');
-  }
-});
 
-function showDate() {
-  var d = new Date();
-  var curr_date = d.getDate();
-  var curr_month = d.getMonth() + 1; //months are zero based
-  var curr_year = d.getFullYear();
-  document.write(curr_date + "-" + curr_month + "-" + curr_year);
-}
